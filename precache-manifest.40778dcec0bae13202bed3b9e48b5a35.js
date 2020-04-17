2a2fab4dae63e2048c092ad975b4a37cbb25f0a2
unknown <valentasveitas@yahoo.com>
2020-04-17 19:26:26 +0300

    Updates

---
+++ precache-manifest.40778dcec0bae13202bed3b9e48b5a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a01de91a682ddac77b78d677a77a82b",
+    "url": "/typescript-cardslist/index.html"
+  },
+  {
+    "revision": "6887ceab743ddda899c1",
+    "url": "/typescript-cardslist/static/css/main.79b919ab.chunk.css"
+  },
+  {
+    "revision": "662cfc3582f3d0e458b5",
+    "url": "/typescript-cardslist/static/js/2.706c926f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-cardslist/static/js/2.706c926f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6887ceab743ddda899c1",
+    "url": "/typescript-cardslist/static/js/main.b48f884b.chunk.js"
+  },
+  {
+    "revision": "4ee39d5cb8d084d6663c",
+    "url": "/typescript-cardslist/static/js/runtime-main.48fd3efc.js"
+  }
+]);
